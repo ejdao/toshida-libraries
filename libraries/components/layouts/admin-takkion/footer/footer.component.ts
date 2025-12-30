baea7ecad42719848b85137d067cc7bd6963820d
ejdao <ejdao2022@gmail.com>
2025-12-30 09:49:18 -0500

    :fire: AdminTakkion descartado

--- libraries/components/layouts/admin-takkion/footer/footer.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  standalone: false,
-  selector: 'tak-footer',
-  templateUrl: './footer.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class TakFooterComponent {}
--- src/app/components/home/component.ts
+++ src/app/components/home/component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  TsdDateRangeFieldComponent,
+  TsdGeneralFieldComponent,
+} from '@toshida/ng-components/fields';
 
 @Component({
-  standalone: true,
+  imports: [TsdGeneralFieldComponent, TsdDateRangeFieldComponent, ReactiveFormsModule, FormsModule],
   selector: 'app-home',
   templateUrl: './component.html',
   styleUrl: './component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  date = new FormControl(new Date());
+  inicio = new FormControl(new Date());
+  fin = new FormControl(new Date());
+
+  texto = new FormControl(500000);
+
+  constructor() {
+    this.texto.valueChanges.subscribe((el) => {
+      console.log(el);
+    });
+  }
+}
